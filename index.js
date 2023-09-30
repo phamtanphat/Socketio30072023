da8ad7cbd802d5da4e409c09c12922830b136929
phamtanphat <phatdroid94@gmail.com>
2023-09-30 16:41:46 +0700

    phan biet get va post

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const app = express()
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-// Tương tác với thanh địa chỉ
-// Có 2 cách:
+// Method Get: Tương tác với thanh địa chỉ
+
 // 1: Truyền theo query
+// app.get("/search", (req, res) => {
+//     let {id, address} = req.query
+//     console.log(id, address)
+// })
 
-app.get("/search", (req, res) => {
-    console.log(req.query)
-})
+// 2: Truyền theo params
+// app.get("/page/:index/:a", (req, res) => {
+//     let { index, a } = req.params
+//     console.log(index, a)
+// })
+
+// Method Post: Dữ liệu khi gửi lên sẽ bảo mật hơn
+// app.post("/login", (req, res) => {
+//     let { email, password } = req.body
+//     console.log(email, password)
+// })
 
 app.listen(3000)
